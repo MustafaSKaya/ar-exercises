879172f17028aca84da65ac3f7859489cae1f9bc
MustafaSKaya <mustafakya88@hotmail.com>
2022-02-01 20:16:24 -0500

    final commit

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
     has_many :employees
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { only_integer: true }
+    validates :apparel
+  
+    def apparel
+      if mens_apparel.blank? && womens_apparel.blank?
+        errors.add(:mens_apparel, "at least one kind of apparel must be carried")
+      end
+
+    end
+
 end
